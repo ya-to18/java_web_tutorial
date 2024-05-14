0c778451afcb2a4bb4d2a011cc3cac82be631553
Yasutake Tomoya <ty0108.98@icloud.com>
2024-05-14 16:33:25 +0900

    creationPreparation

---
+++ WEB-INF/src/forms/ConfirmServlet.java
@@ -0,0 +1,16 @@
+package forms;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ConfirmServlet extends HttpServlet {
+    // POSTメソッドのリクエスト受信時に実行されるメソッド
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+
+    }
+}
